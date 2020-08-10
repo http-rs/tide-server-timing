4ecdeed4e160b60132222641a08c2331d920090a
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-08-10 13:12:36 +0200

    Update main.rs

--- examples/main.rs
+++ examples/main.rs
@@ -2,6 +2,10 @@ use tide_server_timing::{TimingLayer, TimingMiddleware};
 use tracing_core::Level;
 use tracing_subscriber::layer::SubscriberExt;
 
+use async_std::task;
+use std::time::Duration;
+use tracing_futures::Instrument;
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let sub = tracing_subscriber::fmt()
@@ -13,7 +17,14 @@ async fn main() -> tide::Result<()> {
 
     let mut app = tide::new();
     app.with(TimingMiddleware::new());
-    app.at("/").get(|_| async move { Ok("Hello chashu") });
+    app.at("/").get(|_| async move {
+        async move {
+            task::sleep(Duration::from_millis(10)).await;
+            Ok("Hello chashu")
+        }
+        .instrument(tracing::info_span!("my cool span"))
+        .await
+    });
     app.listen("localhost:8080").await?;
     Ok(())
 }
